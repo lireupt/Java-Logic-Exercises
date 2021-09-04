9398ec84b05b6d84910cf0416e0181e6badb7eb1
lireupt <lireu.pt@gmail.com>
2021-09-04 12:56:27 +0100

    Method sum

--- src/SumIntList.java
+++ src/SumIntList.java
@@ -1,3 +1,16 @@
 public class SumIntList {
+    /* Method
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<Integer> ar) {
     
+        Integer sum =0;
+        for(Integer i : ar){
+             sum +=i;
+        }
+        return  sum;  
+        }
 }
+     
